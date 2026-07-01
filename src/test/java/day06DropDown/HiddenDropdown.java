f6ced3918f9a2ef9bfd93c4083425774a5f5782b
Md Ashif <mdashifraza222@gmail.com>
2026-07-01 19:55:27 +0530

    change packeg name

---
+++ src/test/java/day06DropDown/HiddenDropdown.java
@@ -0,0 +1,30 @@
+package day06DropDown;
+
+import java.net.URL;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class HiddenDropdown {
+	public static void main(String[] args) {
+        // Launch Firefox browser
+        WebDriver driver = new FirefoxDriver();
+
+        // Set Implicit Wait for 10 seconds
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Create URL object for OrangeHRM
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+        
+        driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
+        
+        driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("admin123");
+        
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        
+        driver.findElement(By.xpath("//span[text()='PIM']")).click();
+        
+	}
+}
